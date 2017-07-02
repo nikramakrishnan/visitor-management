8ef4b818492987aeb45efefbc87c8c9f60f20017
nikramakrishnan <ramakrishnan.nikhil@gmail.com>
2017-07-02 16:18:18 +0530

    Updated DB tables with more columns

--- vms(v4.0).sql
+++ vms(v4.0).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 27, 2017 at 12:58 PM
+-- Generation Time: Jul 02, 2017 at 12:37 PM
 -- Server version: 10.1.22-MariaDB
 -- PHP Version: 7.1.4
 
@@ -45,7 +45,11 @@ CREATE TABLE `auth_tokens` (
 
 INSERT INTO `auth_tokens` (`id`, `auth_key`, `user_id`, `creation_time`, `expiry`, `last_access`, `device_info`) VALUES
 (3, '7703894a910d71b0f9a5a5a8c44177f15b1210ee', 1, 1498552552, 1501144552, 1498552552, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:55.0) Gecko/20100101 Firefox/55.0'),
-(4, 'eb70437b3e9d948ffa173b9b8da30c9634d6819c', 1, 1498558073, 1501145494, 1498558073, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:55.0) Gecko/20100101 Firefox/55.0');
+(4, 'eb70437b3e9d948ffa173b9b8da30c9634d6819c', 1, 1498558073, 1501145494, 1498558073, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:55.0) Gecko/20100101 Firefox/55.0'),
+(5, '3fc68360514145de65dd4ef3781d5d9d6d7da4b5', 1, 1498646593, 1501238593, 1498646593, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:55.0) Gecko/20100101 Firefox/55.0'),
+(6, 'c650f54090e056095f248f7ad96a89da135af41f', 1, 1498647294, 1501239294, 1498647294, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:55.0) Gecko/20100101 Firefox/55.0'),
+(7, '61bb9d6e463bcfee8b594442cf4d7d3c61be3ad9', 1, 1498665349, 1501257349, 1498665349, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:55.0) Gecko/20100101 Firefox/55.0'),
+(8, 'd05baeac9f41d2ca716cb5d99351359ee91cbe4b', 1, 1498759540, 1501351540, 1498759540, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:55.0) Gecko/20100101 Firefox/55.0');
 
 -- --------------------------------------------------------
 
@@ -84,31 +88,21 @@ CREATE TABLE `visitors` (
   `mobile` varchar(11) NOT NULL,
   `purpose` varchar(50) NOT NULL,
   `photo_ref` varchar(32) NOT NULL,
-  `in_campus` tinyint(1) DEFAULT '1'
+  `entry_time` datetime NOT NULL,
+  `exit_time` datetime DEFAULT NULL,
+  `in_campus` tinyint(1) DEFAULT '1',
+  `added_by` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `visitors`
 --
 
-INSERT INTO `visitors` (`visitor_no`, `card_no`, `name`, `mobile`, `purpose`, `photo_ref`, `in_campus`) VALUES
-(3, 12, 'mike', '944552832', '1', '2383594d563f1d802144136801.jpg', 1),
-(4, 3, 'Jack', '9123654793', '1', '62509594d626813393897676135.jpg', 1),
-(5, 12, 'Nik', '9123654793', '2', '26341594d653aecf5c401928411.jpg', 1),
-(6, 21, 'Jill', '8743573845', '4', '54376594d706bed87f709245802.jpg', 1),
-(7, 11, 'Atom', '9384940249', '2', '47325594d73a11aa34640848711.jpg', 1),
-(8, 11, 'Atom', '9384940249', '2', '10679594d7455f0610695234416.jpg', 1),
-(9, 13, 'Guy', '999834873', '3', '55167594d75f7a351a698190722.jpg', 1),
-(10, 23, 'Kira', '9193473213', '5', '934075950b86ec6ae8044099239.jpg', 1),
-(11, 123, 'Hello', '483927473', '2', '857859520aeb64e74979172011.png', 1),
-(12, 123, 'Hello', '483927473', '2', '2089659520d29ba00a296809208.png', 1),
-(13, 123, 'Hello', '483927473', '2', '1012159520d4417864834382451.png', 1),
-(14, 1, 'A', '12345665', '2', '49768595211c976c59860455235.jpg', 1),
-(15, 1, 'A', '12345665', '2', '34703595211f80b3a3687189582.jpg', 1),
-(16, 1, 'A', '12345665', '2', '51395952126f64257248904851.jpg', 1),
-(17, 41, 'Nk', '81827373', '3', '1670159522fc721788586784247.jpg', 1),
-(18, 41, 'Nk', '81827373', '3', '959455952301c46d78093566619.jpg', 1),
-(19, 41, 'Nk', '81827373', '3', '7399859523052e2fef233819580.jpg', 1);
+INSERT INTO `visitors` (`visitor_no`, `card_no`, `name`, `mobile`, `purpose`, `photo_ref`, `entry_time`, `exit_time`, `in_campus`, `added_by`) VALUES
+(23, 12, 'Dude', '992822992', '2', '15490595613b2cd17d605684109.jpg', '2017-06-30 14:32:42', NULL, 1, 1),
+(24, 23, 'Lol Guy', '947393782', '2', '5153759563f5cb7700516535631.jpg', '2017-06-30 17:39:00', NULL, 1, 1),
+(25, 123, 'N', '982337484', '1', '11069595643b172427651761974.jpg', '2017-06-30 17:57:29', NULL, 1, 1),
+(26, 12, 'ad', '123123123', '1', '458335958bd4c99bf4933977896.jpg', '2017-07-02 15:00:52', NULL, 1, 1);
 
 --
 -- Indexes for dumped tables
@@ -140,7 +134,7 @@ ALTER TABLE `visitors`
 -- AUTO_INCREMENT for table `auth_tokens`
 --
 ALTER TABLE `auth_tokens`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `users`
 --
@@ -150,7 +144,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `visitors`
 --
 ALTER TABLE `visitors`
-  MODIFY `visitor_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;COMMIT;
+  MODIFY `visitor_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
