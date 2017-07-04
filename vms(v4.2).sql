e2918021ae095abd1030a6add0c90dd161070741
nikramakrishnan <ramakrishnan.nikhil@gmail.com>
2017-07-04 17:57:22 +0530

    Modified mobile number to accept new format and cleaned up

--- vms(v4.2).sql
+++ vms(v4.2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 02, 2017 at 12:37 PM
+-- Generation Time: Jul 04, 2017 at 02:25 PM
 -- Server version: 10.1.22-MariaDB
 -- PHP Version: 7.1.4
 
@@ -45,11 +45,7 @@ CREATE TABLE `auth_tokens` (
 
 INSERT INTO `auth_tokens` (`id`, `auth_key`, `user_id`, `creation_time`, `expiry`, `last_access`, `device_info`) VALUES
 (3, '7703894a910d71b0f9a5a5a8c44177f15b1210ee', 1, 1498552552, 1501144552, 1498552552, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:55.0) Gecko/20100101 Firefox/55.0'),
-(4, 'eb70437b3e9d948ffa173b9b8da30c9634d6819c', 1, 1498558073, 1501145494, 1498558073, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:55.0) Gecko/20100101 Firefox/55.0'),
-(5, '3fc68360514145de65dd4ef3781d5d9d6d7da4b5', 1, 1498646593, 1501238593, 1498646593, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:55.0) Gecko/20100101 Firefox/55.0'),
-(6, 'c650f54090e056095f248f7ad96a89da135af41f', 1, 1498647294, 1501239294, 1498647294, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:55.0) Gecko/20100101 Firefox/55.0'),
-(7, '61bb9d6e463bcfee8b594442cf4d7d3c61be3ad9', 1, 1498665349, 1501257349, 1498665349, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:55.0) Gecko/20100101 Firefox/55.0'),
-(8, 'd05baeac9f41d2ca716cb5d99351359ee91cbe4b', 1, 1498759540, 1501351540, 1498759540, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:55.0) Gecko/20100101 Firefox/55.0');
+(4, 'eb70437b3e9d948ffa173b9b8da30c9634d6819c', 1, 1498558073, 1501145494, 1498558073, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:55.0) Gecko/20100101 Firefox/55.0');
 
 -- --------------------------------------------------------
 
@@ -84,8 +80,8 @@ DROP TABLE IF EXISTS `visitors`;
 CREATE TABLE `visitors` (
   `visitor_no` int(11) NOT NULL,
   `card_no` int(11) NOT NULL,
-  `name` varchar(30) NOT NULL,
-  `mobile` varchar(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `mobile` varchar(16) NOT NULL,
   `purpose` varchar(50) NOT NULL,
   `photo_ref` varchar(32) NOT NULL,
   `entry_time` datetime NOT NULL,
@@ -99,10 +95,9 @@ CREATE TABLE `visitors` (
 --
 
 INSERT INTO `visitors` (`visitor_no`, `card_no`, `name`, `mobile`, `purpose`, `photo_ref`, `entry_time`, `exit_time`, `in_campus`, `added_by`) VALUES
-(23, 12, 'Dude', '992822992', '2', '15490595613b2cd17d605684109.jpg', '2017-06-30 14:32:42', NULL, 1, 1),
-(24, 23, 'Lol Guy', '947393782', '2', '5153759563f5cb7700516535631.jpg', '2017-06-30 17:39:00', NULL, 1, 1),
-(25, 123, 'N', '982337484', '1', '11069595643b172427651761974.jpg', '2017-06-30 17:57:29', NULL, 1, 1),
-(26, 12, 'ad', '123123123', '1', '458335958bd4c99bf4933977896.jpg', '2017-07-02 15:00:52', NULL, 1, 1);
+(30, 9, 'admin', '98827277', '3', '35837595a01897b2a6905697554.jpg', '2017-07-03 14:04:17', NULL, 1, 1),
+(46, 21, 'KewlGuy', '+01-321-223-1521', '5', '45383595b835656820458135802.jpg', '2017-07-04 17:30:22', NULL, 1, 1),
+(47, 21, 'Normal Number Guy', '9821465324', '3', '68090595b83fd4e574003667501.jpg', '2017-07-04 17:33:09', NULL, 1, 1);
 
 --
 -- Indexes for dumped tables
@@ -134,7 +129,7 @@ ALTER TABLE `visitors`
 -- AUTO_INCREMENT for table `auth_tokens`
 --
 ALTER TABLE `auth_tokens`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT for table `users`
 --
@@ -144,7 +139,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `visitors`
 --
 ALTER TABLE `visitors`
-  MODIFY `visitor_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;COMMIT;
+  MODIFY `visitor_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
